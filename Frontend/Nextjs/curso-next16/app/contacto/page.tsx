0860e452d3895a466cf79ce2b60150366b1fe81d
Roger Salgado <rogeryonatan@gmail.com>
2026-07-30 15:09:00 -0400

    Next Js

--- Frontend/Nextjs/curso-next16/app/contacto/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function ContactoPage() {
-  return (
-    <main className="p-8">
-      <h1 className="text-3xl font-bold">Contacto</h1>
-      <p className="mt-2 text-gray-600">Escríbenos a contacto@ejemplo.com</p>
-    </main>
-  );
-}
--- Frontend/Nextjs/curso-next16/app/servicios/desarrollo/page.tsx
+++
@@ -1,8 +0,0 @@
-function DesarrolloPage() {
-  return (
-    <main className="p-8">
-      <h1 className="text-3x1 font-bold ">Servicios de Desarrollo</h1>
-    </main>
-  );
-}
-export default DesarrolloPage;
