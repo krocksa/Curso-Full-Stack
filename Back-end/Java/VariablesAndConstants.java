03d15991e380c59da967c631a6b5e7178aad247c
Roger Salgado <rogeryonatan@gmail.com>
2026-08-12 22:50:18 -0400

    Java

---
+++ Back-end/Java/VariablesAndConstants.java
@@ -0,0 +1,19 @@
+public class VariablesAndConstants {
+    static void main() {
+        //Variables
+        //Puedes declararlas y luego asignarles valor,
+        //O declararlas y asignarles valor de una.
+
+        //String name;
+        String name = "Roger";
+
+        int age = 34;
+        System.out.println(name + age);
+
+        //Constantes
+        //Como buena práctica de programación, las constantes,
+        //se nombran en mayúsculas
+        final String EMAIL = "krocksa@gmail.com";
+        System.out.println(EMAIL);
+    }
+}
