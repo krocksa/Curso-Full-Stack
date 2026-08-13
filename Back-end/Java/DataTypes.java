3c2e61a77103c0a0aa6365305811115ebe18a773
Roger Salgado <rogeryonatan@gmail.com>
2026-08-13 01:36:12 -0400

    Java

---
+++ Back-end/Java/DataTypes.java
@@ -0,0 +1,29 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        // Tipos de datos primitivos => Son todos los tipos en minúscula
+
+        // Char === Caracter de texto
+        char caracter = 'a';
+
+        // Enteros
+        int age = 34;
+
+        // Decimales
+        double height = 1.75;
+
+        // Booleanos
+        boolean bool = true;
+
+        // Otros tipos primitivos numéricos
+        // float, long, byte
+
+        // Aunque un String no es un tipo de dato primitivo, por definición,
+        // es un tipo de dato muy utilizado, lo adoptaremos como un tipo de dato
+        // primitivo,
+        // por su uso muy recurente.
+
+        String name = "Roger";
+    }
+}
+
+// 1:57:41 Operadores de asignación
