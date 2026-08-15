bd6258bac9078f76ab7bd20193f4e0e8881a2928
Roger Salgado <rogeryonatan@gmail.com>
2026-08-14 23:58:31 -0400

    Java

--- Back-end/Java/AsignationOperators.java
+++
@@ -1,6 +0,0 @@
-public class AsignationOperators {
-    public static void main(String[] args) {
-
-    }
-}
-// 1:57:41 Operadores de asignación
---
+++ Back-end/Java/Operators.java
@@ -0,0 +1,23 @@
+public class Operators {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+
+        a = b;
+        /* a = a + 1;//Forma obsoleta y en desuso */
+        // Operadores de asignación directa
+        a += 1;
+        System.out.println(a);
+
+        // Operadores de Comparación (o Relacionales)
+
+        System.out.println(a == b);// Operador de igualdad
+        System.out.println(a > b);// Mayor Que
+        System.out.println(a < b);// Menor Que
+        System.out.println(a >= b);// Mayor o igual Que
+        System.out.println(a <= b);// Menor o igual Que
+
+        // Operadores Lógicos (AND Y OR)
+        // 2:06:09
+    }
+}
