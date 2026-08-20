e2e2414209f804bc052e95e77a044d4ca75bef92
Roger Salgado <rogeryonatan@gmail.com>
2026-08-20 15:11:09 -0400

    Java

--- Back-end/Java/Functions.java
+++ Back-end/Java/Functions.java
@@ -1,5 +1,41 @@
 public class Functions {
     public static void main(String[] args) {
-        //5:05:00
+        sendEmail();
+        sendEmailToUser("rogeryonartan@gmail.com");
+        sendEmailToUser("Roger", "rogeryonatan@gmail.com");
+        sendEmaiWithState("rogeryonatan@gmail.com");
+
+        var state = sendEmaiWithState("");
+        System.out.println(state);
+    }
+
+    // Funciones
+    // También llamadas "Métodos"
+    // Si una función/método es estática,
+    // sólamente puede recibir funciones estáticas, sino,
+    // debe instanciarse la clase desde la función estática primero
+
+    public static void sendEmail() {
+        System.out.println("Email sent.");
+    }
+
+    public static void sendEmailToUser(String email) {
+        System.out.println("Email sent to " + email);
+    }
+
+    public static void sendEmailToUser(String name, String email) {
+        System.out.println("Email sent to " + name + ". The user's email is: " + email);
+    }
+    // Dos funciones pueden tener el mismo nombre,
+    // si su comportamiento es distinto, es decir,
+    // si modificamos el comportamiento de los parámetros.
+    // La anterior se llama "Sobrecarga".
+
+    public static boolean sendEmaiWithState(String email) {
+        if (email.isEmpty()) {
+            return false;
+        }
+        System.out.println("Email sent.");
+        return true;
     }
 }
---
+++ Back-end/Java/POO.java
@@ -0,0 +1,6 @@
+public class POO {
+    public static void main(String[] args) {
+        // Programación Orientada a objetos
+        // 05:26:30
+    }
+}
